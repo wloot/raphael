027ceef7063b7fc100e4c3b1deaf996284432a2e
Demon000 <demonsingur@gmail.com>
2019-12-19 13:02:17 +0100

    fts_521: remove usages of lpm_disable_for_input
    Change-Id: I550f6d02eebb9c5a30d1dbd4e9d909b5e46e9d15
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -151,7 +151,6 @@ static int fts_mode_handler(struct fts_ts_info *info, int force);
 static int fts_chip_initialization(struct fts_ts_info *info, int init_type);
 static const char *fts_get_limit(struct fts_ts_info *info);
 static irqreturn_t fts_event_handler(int irq, void *ts_info);
-extern void lpm_disable_for_input(bool on);
 
 /**
 * Release all the touches in the linux input subsystem
@@ -182,7 +181,6 @@ void release_all_touches(struct fts_ts_info *info)
 #ifdef STYLUS_MODE
 	info->stylus_id = 0;
 #endif
-	lpm_disable_for_input(false);
 }
 
 /**
@@ -3378,7 +3376,6 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 	input_mt_report_slot_state(info->input_dev, tool, 0);
 	if (info->touch_id == 0) {
 		input_report_key(info->input_dev, BTN_TOUCH, touch_condition);
-		lpm_disable_for_input(false);
 		if (!touch_condition)
 			input_report_key(info->input_dev, BTN_TOOL_FINGER, 0);
 #ifdef CONFIG_FTS_FOD_AREA_REPORT
@@ -4040,7 +4037,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 			logError(1, "%s pm_resume_completion timeout, i2c is closed", tag);
 			pm_relax(info->dev);
 			fts_enableInterrupt();
-			lpm_disable_for_input(false);
 			return;
 		} else {
 			logError(1, "%s pm_resume_completion be completed, handling irq", tag);
@@ -4095,7 +4091,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 #endif
 	pm_relax(info->dev);
 	fts_enableInterrupt();
-	lpm_disable_for_input(false);
 	return;
 }
 
@@ -4133,7 +4128,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 		return IRQ_HANDLED;
 	}
 #endif
-	lpm_disable_for_input(true);
 
 	info->irq_status = true;
 	error = fts_writeReadU8UX(regAdd, 0, 0, data, FIFO_EVENT_SIZE,
@@ -4178,8 +4172,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 	}
 	input_sync(info->input_dev);
 	info->irq_status = false;
-	if (!info->touch_id)
-		lpm_disable_for_input(false);
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 	wake_up(&info->wait_queue);
 #endif
@@ -5582,7 +5574,6 @@ static void fts_suspend_work(struct work_struct *work)
 	sysfs_notify(&fts_info->fts_touch_dev->kobj, NULL,
 		     "touch_suspend_notify");
 #endif
-	lpm_disable_for_input(false);
 }
 
 #ifdef CONFIG_DRM
