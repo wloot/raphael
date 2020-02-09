ecbab4bda222d3711069e425e7f7251381ed3518
Julian Liu <wlootlxt123@gmail.com>
2020-02-10 01:57:34 +0800

    qca-wifi-host-cmn: Import xiaomi changes
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ drivers/staging/qca-wifi-host-cmn/umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -132,11 +132,7 @@ bool util_is_scan_entry_match(
 	if (entry1->cap_info.wlan_caps.ess &&
 	   !qdf_mem_cmp(entry1->bssid.bytes,
 	   entry2->bssid.bytes, QDF_MAC_ADDR_SIZE)) {
-		/* Check for BSS */
-		if (util_is_ssid_match(&entry1->ssid, &entry2->ssid) ||
-		    util_scan_is_null_ssid(&entry1->ssid) ||
-		    util_scan_is_null_ssid(&entry2->ssid))
-			return true;
+		return true;
 	} else if (entry1->cap_info.wlan_caps.ibss &&
 	   (entry1->channel.chan_idx ==
 	   entry2->channel.chan_idx)) {
