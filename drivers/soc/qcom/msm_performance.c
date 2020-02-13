28584bb421c5065c72771f6220a3f18187cf924a
Julian Liu <wlootlxt123@gmail.com>
2020-02-14 07:48:46 +0800

    msm_performance: Disable userspace boost
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -55,6 +55,7 @@ static unsigned int aggr_top_load;
 /*******************************sysfs start************************************/
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -107,6 +108,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }
