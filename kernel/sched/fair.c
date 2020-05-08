5cb1c1386d5d9a36feb702d73105608d68c60bc9
Julian Liu <wlootlxt123@gmail.com>
2020-05-09 03:15:43 +0800

    sched/fair: Don't skip capacity check for prefer_idle tasks

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7598,12 +7598,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * Ensure minimum capacity to grant the required boost.
 			 * The target CPU can be already at a capacity level higher
 			 * than the one required to boost the task.
-			 * However, if the task prefers idle cpu and that
-			 * cpu is idle, skip this check.
 			 */
 			new_util = max(min_util, new_util);
-			if (!(prefer_idle && idle_cpu(i))
-				&& new_util > capacity_orig)
+			if (new_util > capacity_orig)
 				continue;
 
 			/*
